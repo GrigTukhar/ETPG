a29113d4a02c11746e595c148f49305749d2b239
GrigTukhar <48716024+GrigTukhar@users.noreply.github.com>
2019-04-15 12:13:50 +0400

    Update main.py

--- main.py
+++ main.py
@@ -36,11 +36,21 @@ def printWorkouts(curr_work,day):
 
 
 def main():
-    week = input(str("Input week: "))
-    day = input(str("Input day: "))
-    schedule = loadData(week,day)
-    preworkouts = loadPredef()
-    current_workout = currDay(schedule,day,preworkouts)
-    saveWorkouts(current_workout)
-    printWorkouts(current_workout,day)
+    week = "x"
+    while week !="week1" and week !="week2" and week !="week3" and week !="week4" and week !="week5":
+        week = input(str("Input week (week1, week2, week 3, week 4, or week5): "))
+        if week !="week1" and week !="week2" and week !="week3" and week !="week4" and week !="week5":
+            print("That does not exist, try again")
+        else:
+            day ="x"
+            while day!="day1" and day!="day2" and day!="day3" and day!="day4" and day!="day5" and day!="day6" and day!="day7":
+                day = input(str("Input day (day1, day 2, day 3, day4, day5, day6, or day7): "))
+                if day!="day1" and day!="day2" and day!="day3" and day!="day4" and day!="day5" and day!="day6" and day!="day7":
+                    print("That does not exist, try again")
+                else:
+                    schedule = loadData(week,day)
+                    preworkouts = loadPredef()
+                    current_workout = currDay(schedule,day,preworkouts)
+                    saveWorkouts(current_workout)
+                    printWorkouts(current_workout,day)
 main()
