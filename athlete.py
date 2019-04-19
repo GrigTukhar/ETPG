a62632afc26ffabe145b3e8029ed766115c84a54
GrigTukhar <48716024+GrigTukhar@users.noreply.github.com>
2019-04-19 14:17:43 +0400

    Update athlete.py

--- athlete.py
+++ athlete.py
@@ -1,6 +1,9 @@
 import json
+import time
+
 
 days = ("day1", "day2", "day3", "day4", "day5", "day6", "day7")
+times =("morning", "lunch", "day", "evening")
 
 def loadData():
     with open('main.json') as data_file:
@@ -19,6 +22,11 @@ def loadPredef():
     preworkouts = predata["workouts"]
     return(preworkouts)
 
+def loadFeedback():
+    with open('feedback.json') as data_file:
+        preFeedback = json.load(data_file)
+    return (preFeedback)
+
 def currDay(schedule,day,preworkouts):
     if schedule == 0:
         return 0
@@ -28,7 +36,6 @@ def currDay(schedule,day,preworkouts):
             curr_work[day][key] = preworkouts[schedule[key]["predefinedworkout"]]
         else:
             curr_work[day][key] = schedule[key]
-
     return curr_work
 
 def saveWorkouts(workouts):
@@ -51,6 +58,38 @@ def printWorkouts(curr_work,day):
             else:
                 print(key, ":",curr_work[day][key])
 
+def feedback(curr_work,day,week, preFeedback):
+    answer = "x"
+    time_answer = "x"
+    date = time.time()
+    feedback_work = preFeedback
+    if curr_work!=0:
+        while (answer != "no"):
+            answer = input("Would you like to provide feedback for a workout? yes/no: ")
+            if (answer != "yes" and answer != "no"):
+                print("Invalid input, try again")
+            elif(answer == "yes"):
+                while time_answer not in times:
+                    time_answer = input("Input time of day (" + (", ").join(times) + "): ")
+                    if time_answer not in times:
+                        print("That does not exist, try again")
+                    else:
+                        if curr_work[day][time_answer]== -1:
+                            print("No workout for this time of day, cannot provide feedback")
+                        else:
+                            ifDone = "x"
+                            while (ifDone != "yes" and ifDone != "no"):
+                                ifDone = str(input("Have you completed the workout? yes/no: "))
+                                if (ifDone != "yes" and ifDone != "no"):
+                                    print("Invalid input, try again")
+                            text_feedback = input("Input feedback (if you did the workout how you felt, if not why): ")
+                            feedback_work[date] = {week: {day: {time_answer: {}, "hasCompleted": {}, "feedback": {}}}}
+                            feedback_work[date][week][day][time_answer] = curr_work[day][time_answer]
+                            feedback_work[date][week][day]["hasCompleted"] = ifDone
+                            feedback_work[date][week][day]["feedback"] = text_feedback
+                            file = open("feedback.json", "w")
+                            file.write(json.dumps(feedback_work ))
+                            file.close()
 
 def main():
     week = "x"
@@ -68,8 +107,10 @@ def main():
                     print("That does not exist, try again")
                 else:
                     week_schedule =loadDataForWeekAndDay(full_schedule,week,day)
-                    preworkouts = loadPredef()
-                    current_workout = currDay(week_schedule,day,preworkouts)
+                    preWorkouts = loadPredef()
+                    preFeedback = loadFeedback()
+                    current_workout = currDay(week_schedule,day,preWorkouts)
                     saveWorkouts(current_workout)
                     printWorkouts(current_workout,day)
+                    feedback(current_workout,day,week,preFeedback)
 main()
