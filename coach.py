0deda799fc0496d4c777051d7999f88572eccb6c
GrigTukhar <48716024+GrigTukhar@users.noreply.github.com>
2019-05-06 17:38:26 +0400

    Update coach.py

--- coach.py
+++ coach.py
@@ -20,37 +20,39 @@ def loadPredef():
     return(preworkouts)
 
 def editWeek(data):
-    count = 1
-    reply = "x"
-    print("Current weeks in schedule :(" + (", ").join(data["schedule"].keys()) + ")")
-    for key in data["schedule"]:
-        count = count + 1
 
-    while (reply not in replies_week):
+    reply = "x"
+    print("\nCurrent weeks in schedule :(" + (", ").join(data["schedule"].keys()) + ")")
+
+
+    while (reply not in replies_week) or reply!="exit":
+        count = 1
+        for key in data["schedule"]:
+            count = count + 1
         reply = str(input("What would you like to do with the weeks (" + (", ").join(replies_week) + "): "))
         if (reply not in replies_week):
             print("Invalid input, try again")
 
-    if (reply == "add"):
-        count = str(count)
-        print("week"+count, "has been added")
-        data["schedule"]["week"+count] = { "day1": { "morning": -1, "lunch": -1, "day": -1, "evening": -1 }, "day2": { "morning": -1, "lunch": -1, "day": -1, "evening": -1 }, "day3": { "morning": -1, "lunch": -1, "day": -1, "evening": -1 }, "day4": { "morning": -1, "lunch": -1, "day": -1, "evening": -1 }, "day5": { "morning": -1, "lunch": -1, "day": -1, "evening": -1 }, "day6": { "morning": -1, "lunch": -1, "day": -1, "evening": -1 }, "day7": { "morning": -1, "lunch": -1, "day": -1, "evening": -1 } }
+        if (reply == "add"):
+            count = str(count)
+            print("\nweek"+count, "has been added\n")
+            data["schedule"]["week"+count] = { "day1": { "morning": -1, "lunch": -1, "day": -1, "evening": -1 }, "day2": { "morning": -1, "lunch": -1, "day": -1, "evening": -1 }, "day3": { "morning": -1, "lunch": -1, "day": -1, "evening": -1 }, "day4": { "morning": -1, "lunch": -1, "day": -1, "evening": -1 }, "day5": { "morning": -1, "lunch": -1, "day": -1, "evening": -1 }, "day6": { "morning": -1, "lunch": -1, "day": -1, "evening": -1 }, "day7": { "morning": -1, "lunch": -1, "day": -1, "evening": -1 } }
 
-    if (reply == "remove"):
-        count = count - 1
-        count = str(count)
-        print("week"+count, "has been removed")
-        del data["schedule"]["week" + count]
-    file = open("main.json", "w")
-    file.write(json.dumps(data))
-    file.close()
+        if (reply == "remove"):
+            count = int(count) - 1
+            count = str(count)
+            print("\nweek"+count, "has been removed\n")
+            del data["schedule"]["week" + count]
+        file = open("main.json", "w")
+        file.write(json.dumps(data))
+        file.close()
 
 def editWorkout(data,preworkouts):
     week = "x"
     time = "x"
     reply = "x"
     while week not in data["schedule"].keys():
-        week = input(str("Input week (" + (", ").join(data["schedule"].keys()) + "): "))
+        week = input(str("\nInput week (" + (", ").join(data["schedule"].keys()) + "): "))
         if week not in data["schedule"].keys():
             print("That does not exist, try again")
         else:
@@ -61,51 +63,58 @@ def editWorkout(data,preworkouts):
                     print("That does not exist, try again")
 
     week_schedule = data["schedule"][week][day]
-    print("Currently looking at workouts on",day, "in",week, ":", week_schedule)
+    print("\nCurrently looking at workouts on",day, "in",week,"\n")
+    for key in week_schedule:
+        if week_schedule[key] == -1:
+            print(key, ":", "no excerisize for this time of day")
+        elif "predefinedworkout" in week_schedule[key].keys():
+            print(key, ":", "predefined workout -",week_schedule[key]["predefinedworkout"])
+        else:
+            print(key, ":", week_schedule[key]["type"], "for", week_schedule[key]["minutes"], "minutes and",
+                  week_schedule[key]["distance"], "kilometers, at a", week_schedule[key]["load"], "load")
+
     while time not in times:
-        time =input(str("Input time of day (" + (", ").join(times) + "): "))
+        time =input(str("\nInput time of day (" + (", ").join(times) + "): "))
         if time not in times:
             print("That does not exist, try again")
     if (week_schedule[time]==-1):
         print("No workout planned for",time)
-    else:
-        print("Workout planned for",time,"=",week_schedule[time])
     while (reply not in replies_workout):
-        reply = str(input("Choose an option to perform on this workout (" + (", ").join(replies_workout) + "): "))
+        reply = str(input("\nChoose an option to perform on this workout (" + (", ").join(replies_workout) + "): "))
         if (reply not in replies_workout):
             print("Invalid input, try again")
 
     if(reply == "remove"):
         if (week_schedule[time]==-1):
-            print(time, "is already empty")
+            print("\nThe time:",time, "is already empty")
         else:
             data["schedule"][week][day][time]=-1
-            print("Workout during", time, "on", day,"in",week,"has been removed")
+            print("\nWorkout during", time, "on", day,"in",week,"has been removed")
 
     if(reply == "edit"):
         ask = "x"
         workout = "x"
         while (ask != "yes" and ask != "no"):
-            ask = str(input("Would you like to add a predefined workout? yes/no: "))
+            ask = str(input("\nWould you like to add a predefined workout? yes/no: "))
             if (ask != "yes" and ask != "no"):
                 print("Invalid input, try again")
 
         if (ask == "yes"):
             while (workout not in preworkouts.keys()) or workout == "test":
-                workout = input(str("Input premade workout, except for 'test' (" + (", ").join(preworkouts.keys()) + "): "))
+                workout = input(str("\nInput premade workout, except for 'test' (" + (", ").join(preworkouts.keys()) + "): "))
                 if workout == "test":
                     print("Cannot use test")
                 elif (workout not in preworkouts.keys()):
                     print("That does not exist, try again")
                 else:
                     data["schedule"][week][day][time] = {"predefinedworkout": workout}
-                    print("Premade workout",workout,"has been placed during",time,"on", day,"in",week)
+                    print("\nPremade workout",workout,"has been placed during",time,"on", day,"in",week)
 
         if (ask == "no"):
             sport = "x"
             load = "x"
             while (sport not in sports):
-                sport = str(input("Choose sport type (" + (", ").join(sports) + "): "))
+                sport = str(input("\nChoose sport type (" + (", ").join(sports) + "): "))
                 if (sport not in sports):
                     print("Invalid input, try again")
 
@@ -134,25 +143,29 @@ def editWorkout(data,preworkouts):
                     print("Wrong Input: Please input a number")
 
             while (load not in loads):
-                load = str(input("Choose sport type (" + (", ").join(loads) + "): "))
+                load = str(input("Choose load (" + (", ").join(loads) + "): "))
                 if (load not in loads):
                     print("Invalid input, try again")
             data["schedule"][week][day][time] = {"type": sport, "minutes": minutes, "distance": distance, "load": load}
-            print("Workout has been made during",time,"on",day,"in",week,":", week_schedule[time])
+            print("Workout created during",time,"on",day,"in",week, ":", data["schedule"][week][day][time]["type"], "for", data["schedule"][week][day][time]["minutes"], "minutes and",
+                  data["schedule"][week][day][time]["distance"], "kilometers, at a", data["schedule"][week][day][time]["load"], "load")
     file = open("main.json", "w")
     file.write(json.dumps(data))
     file.close()
 
 def main():
-    data = loadData()
-    preworkouts = loadPredef()
     answer = "x"
     while (answer not in answers) or answer !="exit":
-        answer = str(input("Coach Menu | What would you like to edit in the schedule (" + (", ").join(answers) + "): "))
+        answer = str(input("\nCoach Menu | What would you like to edit in the schedule (" + (", ").join(answers) + "): "))
         if (answer not in answers):
             print("Invalid input, try again")
         elif (answer=="week"):
+            data = loadData()
             editWeek(data)
         elif (answer=="workout"):
+            data = loadData()
+            preworkouts = loadPredef()
             editWorkout(data,preworkouts)
+
+
 
