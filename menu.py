b6b89f0c05ef73bb3e7dffc4c47f9a9b6068c9fe
GrigTukhar <48716024+GrigTukhar@users.noreply.github.com>
2019-05-15 11:58:23 +0400

    Update menu.py

--- menu.py
+++ menu.py
@@ -7,9 +7,9 @@ choices = ("athlete", "coach", "predefined", "exit")
 
 def main():
     choice = "x"
-    print("ETPG® | The training schedule program for any endurance trainer and athlete")
+    print("\nETPG® | The training schedule program for any endurance trainer and athlete")
     while (choice not in  choices) or choice !="exit":
-        choice = str(input("General Menu | choose any of the following options (" + (", ").join(choices) + "): "))
+        choice = str(input("\nGeneral Menu | Choose any of the following options (" + (", ").join(choices) + "): "))
         if (choice not in choices):
             print("Invalid input, try again")
         elif (choice == "athlete"):
@@ -18,4 +18,7 @@ def main():
             coach.main()
         elif (choice == "predefined"):
             predefined.main()
+        elif (choice =="exit"):
+            print("\nThank you for using ETPG® | The training schedule program for any endurance trainer and athlete")
+
 main()
